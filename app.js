59e303352b91c56c33b4672aca75384e63d4950a
Marcelo_Bettini <marcelobettini@yahoo.com.ar>
2023-11-06 20:31:52 -0300

    test deploy

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+app.use(express.static("public"));
+const PORT = process.env.PORT ?? 3000;
+
+app.listen(PORT, () => {
+  console.log("Servidor funcionando en http://localhost:" + PORT);
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/home.html");
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile(__dirname + "/views/login.html");
+});
+
+app.get("/register", (req, res) => {
+  res.sendFile(__dirname + "/views/register.html");
+});
